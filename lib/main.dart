03cddccd7abbfb4c71c5bb82d1a63362408633b9
Waqad <waqadarshad2@gmail.com>
2025-09-01 19:22:35 +0500

    - added internet permission - added launcher icon and package rename setups - added force update helper which works with an external config which we can add on our own server and add a UI to handle that from the Admin Dashboard. - added some more useful packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:force_update_helper/force_update_helper.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'core/widgets/dialogs/show_alert_dialog.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +19,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _rootNavigatorKey,
+      builder: (context, child) {
+        return ForceUpdateWidget(
+          navigatorKey: _rootNavigatorKey,
+          forceUpdateClient: ForceUpdateClient(
+            // * Real apps should fetch this from an API endpoint or via
+            // * Firebase Remote Config
+            // + TODO: change this once tested for the first time, to 1.0.0 otherwise will shhow the dialog everytime
+            fetchRequiredVersion: () => Future.value('2.0.0'),
+            // * Example ID from this app: https://fluttertips.dev/
+            // * To avoid mistakes, store the ID as an environment variable and
+            // * read it with String.fromEnvironment
+            iosAppStoreId: '6482293361',
+          ),
+          allowCancel: false,
+          showForceUpdateAlert: (context, allowCancel) => showAlertDialog(
+            context: context,
+            title: 'App Update Required',
+            content: 'Please update to continue using the app.',
+            cancelActionText: allowCancel ? 'Later' : null,
+            defaultActionText: 'Update Now',
+          ),
+          showStoreListing: (storeUrl) async {
+            if (await canLaunchUrl(storeUrl)) {
+              await launchUrl(
+                storeUrl,
+                // * Open app store app directly (or fallback to browser)
+                mode: LaunchMode.externalApplication,
+              );
+            } else {
+              log('Cannot launch URL: $storeUrl');
+            }
+          },
+          onException: (e, st) {
+            log(e.toString());
+          },
+          child: child!,
+        );
+      },
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
