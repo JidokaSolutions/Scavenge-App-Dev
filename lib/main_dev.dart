13226302c9c49e8063a000911a378a760165cb74
Waqad <waqadarshad2@gmail.com>
2025-09-03 03:44:19 +0500

    - re-initialized shorebird - added some more commands for shorebird based releases and patches - added shorebird_code_push package to read the patch number and set that in Sentry also, for better error tracing

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
 import 'app.dart';
 import 'core/env/env.dart';
@@ -17,6 +18,8 @@ Future<void> main() async {
 
   Env.setEnv(env.isNotEmpty ? env : EnvType.dev);
 
+  // Get the current patch number. This will be null if no patch is installed.
+  final patch = await ShorebirdUpdater().readCurrentPatch();
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
@@ -37,30 +40,45 @@ Future<void> main() async {
       // you can also configure SENTRY_DSN, SENTRY_RELEASE, SENTRY_DIST, and
       // SENTRY_ENVIRONMENT via Dart environment variable (--dart-define)
     },
-    appRunner: () => runApp(
-      ProviderScope(
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: Banner(location: BannerLocation.topStart, color: Colors.redAccent, message: Env.appTag, child: const MyApp()),
+    appRunner: () {
+      if (patch != null) {
+        // Add the patch number as a tag. You can use whatever name you would like
+        // as the key. `$patch` will be "null" if there is no patch. You may
+        // wish to handle this case differently.
+        Sentry.configureScope((scope) {
+          scope.setTag('shorebird_patch_number', '${patch.number}');
+        });
+      }
+      return runApp(
+        ProviderScope(
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Banner(location: BannerLocation.topStart, color: Colors.redAccent, message: Env.appTag, child: const MyApp()),
+          ),
         ),
-      ),
-    ),
+      );
+    },
   );
 
   // Use Sentry implementation
   logger = MySentryLogger();
 
-
   // TODO: Remove this line after sending the first sample event to sentry.
   try {
     throw StateError('Sentry Test Exception');
   } catch (exception, stackTrace) {
-    await Sentry.captureException(
-      exception,
-      stackTrace: stackTrace,
-    );
+    await Sentry.captureException(exception, stackTrace: stackTrace);
   }
 }
 
 //! # Run dev
 //+ flutter run --flavor dev lib/main_dev.dart
+//+ flutter run release --flavor dev lib/main_dev.dart
+//+ flutter build apk --flavor dev lib/main_dev.dart
+
+//! Shorebird
+//+ shorebird release android --target ./lib/main_dev.dart --flavor dev --flutter-version 3.32.8
+//+ shorebird release android --artifact apk --target ./lib/main_dev.dart --flavor dev --flutter-version 3.32.8 '--' --dart-define=env="prod"
+
+//+ shorebird patch android --target ./lib/main_dev.dart --flavor dev --flutter-version 3.32.8
+//+ shorebird patch android --artifact apk --target ./lib/main_dev.dart --flavor dev --flutter-version 3.32.8 '--' --dart-define=env="prod"
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
 import 'app.dart';
 import 'core/env/env.dart';
@@ -17,17 +18,27 @@ Future<void> main() async {
 
   Env.setEnv(env.isNotEmpty ? env : EnvType.prod);
 
-  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((value) {
-  //   runApp(ProviderScope(child: const MyApp()));
-  // });
+  // Get the current patch number. This will be null if no patch is installed.
+  final patch = await ShorebirdUpdater().readCurrentPatch();
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   await SentryFlutter.init((options) {
     options.dsn = 'https://33a86ce2a4d3febaefa52cafe4946993@o4509947602862080.ingest.us.sentry.io/4509951176278016';
     // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
     // We recommend adjusting this value in production.
     options.tracesSampleRate = 0.7;
-  }, appRunner: () => runApp(ProviderScope(child: const MyApp())));
+  }, appRunner: () {
+    if (patch != null) {
+      // Add the patch number as a tag. You can use whatever name you would like
+      // as the key. `$patch` will be "null" if there is no patch. You may
+      // wish to handle this case differently.
+      Sentry.configureScope((scope) {
+        scope.setTag('shorebird_patch_number', '${patch.number}');
+      });
+    }
+    return runApp(ProviderScope(child: const MyApp()));
+  });
 
 
   // Use Sentry implementation
@@ -39,4 +50,12 @@ Future<void> main() async {
 
 //! # Build release artifacts
 //+ flutter build appbundle --flavor prod lib/main_prod.dart
-//+ flutter build apk --flavor dev lib/main_dev.dart
+//+ flutter build apk --flavor prod lib/main_prod.dart
+
+
+//! Shorebird
+//+ shorebird release android  --artifact apk --target ./lib/main_prod.dart --flavor prod --flutter-version 3.32.8
+//+ shorebird release android --artifact apk --target ./lib/main_prod.dart --flavor prod --flutter-version 3.32.8 '--' --dart-define=env="prod"
+
+//+ shorebird patch android  --artifact apk --target ./lib/main_prod.dart --flavor prod --flutter-version 3.32.8
+//+ shorebird patch android --artifact apk --target ./lib/main_prod.dart --flavor prod --flutter-version 3.32.8 '--' --dart-define=env="prod"
