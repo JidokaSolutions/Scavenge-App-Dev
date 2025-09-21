546e4fb91639414d1c30021a695a42c2eabd25e2
Waqad <waqadarshad2@gmail.com>
2025-09-21 05:12:56 +0500

    added some more sizes to be used in different places throughout the app which would make it easier for us to replace specific heights throughout the app, if need be

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppSizes {
+  // EdgeInsets constants
   static const DEFAULT = EdgeInsets.symmetric(horizontal: 16, vertical: 16);
   static const HORIZONTAL = EdgeInsets.symmetric(horizontal: 16);
   static const VERTICAL = EdgeInsets.symmetric(vertical: 16);
@@ -9,4 +10,30 @@ class AppSizes {
     horizontal: 16,
   );
   static const ZERO = EdgeInsets.zero;
+
+  // Height constants
+  static const double HEIGHT_5 = 5.0;
+  static const double HEIGHT_8 = 8.0;
+  static const double HEIGHT_10 = 10.0;
+  static const double HEIGHT_12 = 12.0;
+  static const double HEIGHT_15 = 15.0;
+  static const double HEIGHT_16 = 16.0;
+  static const double HEIGHT_20 = 20.0;
+  static const double HEIGHT_24 = 24.0;
+  static const double HEIGHT_25 = 25.0;
+  static const double HEIGHT_30 = 30.0;
+  static const double HEIGHT_32 = 32.0;
+  static const double HEIGHT_40 = 40.0;
+  static const double HEIGHT_50 = 50.0;
+
+  // Width constants
+  static const double WIDTH_5 = 5.0;
+  static const double WIDTH_8 = 8.0;
+  static const double WIDTH_10 = 10.0;
+  static const double WIDTH_12 = 12.0;
+  static const double WIDTH_15 = 15.0;
+  static const double WIDTH_16 = 16.0;
+  static const double WIDTH_20 = 20.0;
+  static const double WIDTH_24 = 24.0;
+  static const double WIDTH_30 = 30.0;
 }
