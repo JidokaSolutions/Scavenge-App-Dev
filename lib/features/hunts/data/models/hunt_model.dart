4d71a3f96793c76ffec3a95fb2d01c3679514ae4
Waqad <waqadarshad2@gmail.com>
2025-09-21 05:06:45 +0500

    removed old placeholder hunts model

--- lib/features/hunts/data/models/hunt_model.dart
+++
@@ -1,9 +0,0 @@
-class Hunt {
-  final String huntName;
-
-  Hunt({this.huntName = ""});
-
-  Hunt copyWith({final String? huntName}) => Hunt(huntName: huntName ?? this.huntName);
-
-  factory Hunt.fromJson(Map<String, dynamic> json) => Hunt(huntName: json['hunt_name']);
-}
